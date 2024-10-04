ad07f137eae436f39e33c2c560c48b71c0a85b14
xiuxiu62 <jacremer@live.com>
2024-10-03 19:05:42 -0700

    starts setting up general.el

--- init.el
+++ init.el
@@ -7,22 +7,33 @@
 (defvar init-file-path
   (pcase system-type
     ('windows-nt "C:/Users/jacre/AppData/Roaming/.emacs.d/init.el")
-    ('gnu/linux "/home/xiuxiu/.config/.emacs.d/init.el")
+    ('gnu/linux "/home/xiuxiu/.emacs.d/init.el")
     (_ "")))
 (add-hook 'emacs-startup-hook (lambda () (find-file init-file-path)))
+
+;; Disable default ui
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+(column-number-mode 1)
+(electric-pair-mode 1) ;; auto close parens
+(show-paren-mode 1)    ;; show matching paren
+(fido-vertical-mode 1) ;; better minibuffer completions
+
+(global-display-line-numbers-mode)
+
+;; Save minibuffer history
+(savehist-mode 1) ;; TODO: maybe remove
 
 (setq inhibit-startup-message t           ;; Disable splash screen
       visible-bell t                      ;; Disable bell sound
       make-backup-files nil               ;; Disable backup files
       auto-save-default nil               ;; Disable auto save files
       history-length 25                   ;; Set minibuffer history size
+      display-line-numbers-type 'relative
       mouse-wheel-scroll-amount '(3 ((shift) . 1))  ;; Scroll slower when shift is held
-      mouse-wheel-progressive-speed nil)
-
-(electric-pair-mode 1) ;; auto close parens
-(show-paren-mode 1)    ;; show matching paren
-(ido-mode 1)           ;; better buffer and file switching
-(fido-mode 1)          ;; better minibuffer completions
+      mouse-wheel-progressive-speed nil
+      custom-file (make-temp-file "emacs-custom"))
 
 (add-hook 'prog-mode-hook 'flymake-mode)
 
@@ -30,29 +41,40 @@
 ;; (defvar recentf-max-menu-items 25)
 ;; (defvar recentf-max-saved-items 25)
 
-;; Save minibuffer history
-(savehist-mode 1) ;; TODO: maybe remove
-
 ;; Keep track of recently opened files
 ;; (recentf-mode 1) ;; TODO: maybe remove
 ;; (global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
-;; Disable default ui
-(menu-bar-mode 0)
-(tool-bar-mode 0)
-(scroll-bar-mode 0)
-
-(global-display-line-numbers-mode 1)
-
 (load-theme 'modus-vivendi t)
 
 ;; Font options
-(set-face-attribute 'default nil :height 115)
+(set-face-attribute 'default nil :height 110)
 
 ;; Window options
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 (set-frame-parameter (selected-frame) 'alpha '(80 . 70))
 (add-to-list 'default-frame-alist '(alpha . (80 . 70)))
+
+
+(set-face-attribute 'mode-line nil
+		    ;; :background "#dd44dd"
+		    :background "#772277"
+		    :foreground "white"
+		    :height 120
+		    :box `(:line-width (-1 . 2) :color "#772277"))
+
+(setq-default mode-line-format
+      '("%e"
+	mode-line-front-space
+	mode-line-buffer-identification
+	mode-line-modified
+	"  "
+	mode-line-position
+	vc-mode
+	"  "
+ 	mode-line-modes
+	mode-line-misc-info
+	mode-line-end-spaces))
 
 (require 'package)
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
@@ -62,31 +84,65 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(defun install (package)
+(defmacro install (&rest packages)
   "Install a package if it isn't already installed."
-  (unless (package-installed-p package)
-    (package-install package)))
-
-(defun install-many (packages)
-  "Installs packages they aren't already installed."
-  (mapc #'install packages))
+  `(mapc (lambda (package)
+	   (unless (package-installed-p package)
+	     (package-install package)))
+	 ',packages))
 
 (defmacro config (package &rest body)
   "Configure a package after it's loaded."
   `(with-eval-after-load 'package
      ,@body))
 
-(install-many '(which-key
-		evil
-		evil-collection))
+(install which-key
+	 general
+	 evil
+	 evil-collection
+	 evil-commentary
+	 magit)
 
-(with-eval-after-load 'which-key
-  '(which-key-mode 1))
+(config which-key
+	(which-key-mode)
+	(setq which-key-idle-delay 0
+	      which-key-idle-secondary-delay 0))
 
 (config evil
 	(setq evil-want-integration t
 	      evil-want-keybinding nil)
 	(evil-mode 1))
+
+(config evil-commentary
+	(evil-commentary-mode))
+
+(config general
+	(general-auto-unbind-keys))
+	;; (general-evil-setup)
+
+(general-create-definer leader
+  :prefix "SPC")
+
+(leader
+  :keymaps 'normal
+  "f" '(:ignore t :wk "file")
+  "ff" '(find-file :wk "find")
+  "x" '(execute-extended-command :wk "commands")
+  ";" 'evil-commentary-line
+  :keymaps 'visual
+  ";" 'evil-commentary-line
+  :keymaps 'emacs
+  "e" '(:ignore t :wk "eval")
+  "eb" '(eval-buffer :wk "buffer")
+  "ee" '(eval-last-sexp :wk "last"))
+
+;; (require 'eglot)
+(config eglot
+	(add-hook 'python-mode-hook 'elgot-ensure)
+	(add-hook 'rust-mode-hook 'elgot-ensure)
+	(setq
+	 eglot-autoshutdown 1
+	 eglot-confirm-server-initiated-edits nil))
 
 ;; Evaluate buffer and reload init file
 (defun reload-init-file ()
